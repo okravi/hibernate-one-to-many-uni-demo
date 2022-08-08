b6294f7c296961fd4d9e74095d70de036fd68ffe
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-08-08 14:34:35 +0300

    progress update

---
+++ src/com/lc/hibernate/demo/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,57 @@
+package com.lc.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.lc.hibernate.demo.entity.Course;
+import com.lc.hibernate.demo.entity.Instructor;
+import com.lc.hibernate.demo.entity.InstructorDetail;
+import com.lc.hibernate.demo.entity.Review;
+
+public class DeleteCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {		
+			
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			// get the course
+			int theId = 10;
+			Course theCourse =session.get(Course.class, theId);
+			
+			//print the course
+			System.out.println(theCourse);
+			
+			//print reviews
+			System.out.println(theCourse.getReviews());
+			
+			//delete
+			session.delete(theCourse);
+			
+			//commit transaction
+			session.getTransaction().commit();;
+			System.out.println("Done");
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
